e1be7c091210b500d4cac7abe2ab67f186628ec1
1338h4x <guywithasword@gmail.com>
2015-10-27 22:06:07 -0400

    Driver activity ported in.

--- app/src/main/java/edu/temple/materialdesigntest/activities/DriverActivity.java
+++ app/src/main/java/edu/temple/materialdesigntest/activities/DriverActivity.java
@@ -1,11 +1,14 @@
 package edu.temple.materialdesigntest.activities;
 
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import edu.temple.materialdesigntest.R;
@@ -21,6 +24,15 @@ public class DriverActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NumberPicker routepic = (NumberPicker)findViewById(R.id.Route);
+        NumberPicker directionpic = (NumberPicker)findViewById(R.id.Direction);
+        routepic.setMinValue(0);
+        routepic.setMaxValue(1);
+        routepic.setDisplayedValues(new String[]{"49", "Temple"});
+        directionpic.setMinValue(0);
+        directionpic.setMaxValue(1);
+        directionpic.setDisplayedValues(new String[]{"Inbound", "Outbound"});
     }
 
     @Override
@@ -53,6 +65,20 @@ public class DriverActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OpenView(View view){
+        //Intent intent = new Intent(this, DriverView.class);
+        NumberPicker routepic = (NumberPicker)findViewById(R.id.Route);
+        NumberPicker directionpic = (NumberPicker)findViewById(R.id.Direction);
+        String route = routepic.getDisplayedValues()[routepic.getValue()];
+        String direction = directionpic.getDisplayedValues()[directionpic.getValue()];
+        //intent.putExtra("route", route);
+        //intent.putExtra("direction", direction);
+        //startActivity(intent);
+
+        //TODO - create the next activity and open it
+        Toast.makeText(this, "Route: " + route + ", Direction: " + direction, Toast.LENGTH_LONG).show();
     }
 
 }
