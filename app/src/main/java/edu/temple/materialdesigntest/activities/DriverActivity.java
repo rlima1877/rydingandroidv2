62c1a856639607e64c3017615c1a8a366d34d09a
Akilese <akil.harmon@gmail.com>
2015-11-02 15:32:26 -0500

    Update Splash screen

--- app/src/main/java/edu/temple/materialdesigntest/activities/DriverActivity.java
+++ app/src/main/java/edu/temple/materialdesigntest/activities/DriverActivity.java
@@ -68,14 +68,14 @@ public class DriverActivity extends AppCompatActivity {
     }
 
     public void OpenView(View view){
-        Intent intent = new Intent(this, DriverView.class);
-        NumberPicker routepic = (NumberPicker)findViewById(R.id.Route);
-        NumberPicker directionpic = (NumberPicker)findViewById(R.id.Direction);
-        String route = routepic.getDisplayedValues()[routepic.getValue()];
-        String direction = directionpic.getDisplayedValues()[directionpic.getValue()];
-        intent.putExtra("route", route);
-        intent.putExtra("direction", direction);
-        startActivity(intent);
+       // Intent intent = new Intent(this, DriverView.class);
+       // NumberPicker routepic = (NumberPicker)findViewById(R.id.Route);
+       // NumberPicker directionpic = (NumberPicker)findViewById(R.id.Direction)
+       // String route = routepic.getDisplayedValues()[routepic.getValue()];
+       // String direction = directionpic.getDisplayedValues()[directionpic.getValue()];
+       // intent.putExtra("route", route);
+       // intent.putExtra("direction", direction);
+       // startActivity(intent);
 
         //TODO - create the next activity and open it
         //Toast.makeText(this, "Route: " + route + ", Direction: " + direction, Toast.LENGTH_LONG).show();
