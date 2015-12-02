4886e96a472f6f2a0752896118e0ab881cbf3865
Rafael <rlima1877@gmail.com>
2015-12-02 13:47:06 -0500

    Implemented Login Screen for Driver.
    -Still need to tie activities together.
    -Still need to connect to database.
    -Still need to create a register screen.

---
+++ app/src/main/java/edu/temple/materialdesigntest/activities/LoginActivity.java
@@ -0,0 +1,300 @@
+package edu.temple.materialdesigntest.activities;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.text.util.Linkify;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.temple.materialdesigntest.R;
+
+
+/**
+ * Android login screen Activity
+ */
+public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    private static final String DUMMY_CREDENTIALS = "admin:admin";
+
+    private UserLoginTask userLoginTask = null;
+    private View loginFormView;
+    private View progressView;
+    private AutoCompleteTextView driverIDTextView;
+    private EditText passwordTextView;
+    private TextView signUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        driverIDTextView = (AutoCompleteTextView) findViewById(R.id.email);
+        loadAutoComplete();
+
+        passwordTextView = (EditText) findViewById(R.id.password);
+        passwordTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_NULL) {
+                    initLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button loginButton = (Button) findViewById(R.id.email_sign_in_button);
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initLogin();
+            }
+        });
+
+        loginFormView = findViewById(R.id.login_form);
+        progressView = findViewById(R.id.login_progress);
+
+        //adding underline and link to signup textview
+        signUpTextView = (TextView) findViewById(R.id.signUpTextView);
+        signUpTextView.setPaintFlags(signUpTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        Linkify.addLinks(signUpTextView, Linkify.ALL);
+
+        signUpTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("LoginActivity", "Sign Up Activity activated.");
+                // this is where you should start the signup Activity
+                // LoginActivity.this.startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+
+    private void loadAutoComplete() {
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+
+    /**
+     * Validate Login form and authenticate.
+     */
+    public void initLogin() {
+        if (userLoginTask != null) {
+            return;
+        }
+
+        driverIDTextView.setError(null);
+        passwordTextView.setError(null);
+
+        String driverID = driverIDTextView.getText().toString();
+        String password = passwordTextView.getText().toString();
+
+        boolean cancelLogin = false;
+        View focusView = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordTextView.setError(getString(R.string.invalid_password));
+            focusView = passwordTextView;
+            cancelLogin = true;
+        }
+
+        if (TextUtils.isEmpty(driverID)) {
+            driverIDTextView.setError(getString(R.string.field_required));
+            focusView = driverIDTextView;
+            cancelLogin = true;
+        } else if (!isDriverIDValid(driverID)) {
+            driverIDTextView.setError(getString(R.string.invalid_driver_id));
+            focusView = driverIDTextView;
+            cancelLogin = true;
+        }
+
+        if (cancelLogin) {
+            // error in login
+            focusView.requestFocus();
+        } else {
+            // show progress spinner, and start background task to login
+            showProgress(true);
+            userLoginTask = new UserLoginTask(driverID, password);
+            userLoginTask.execute((Void) null);
+        }
+    }
+
+    private boolean isDriverIDValid(String driverID) {
+        //add your own logic
+        return driverID.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //add your own logic
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            loginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            progressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> driverIDs = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            driverIDs.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        addDriverIDsToAutoComplete(driverIDs);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private void addDriverIDsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        driverIDTextView.setAdapter(adapter);
+    }
+
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+    /**
+     * Async Login Task to authenticate
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String driverID;
+        private final String passwordStr;
+
+        UserLoginTask(String driverID, String password) {
+            this.driverID = driverID;
+            passwordStr = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //this is where you should write your authentication code
+            // or call external service
+            // following try-catch just simulates network access
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            //using a local dummy credentials store to authenticate
+            String[] pieces = DUMMY_CREDENTIALS.split(":");
+            if (pieces[0].equals(driverID) && pieces[1].equals(passwordStr)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            userLoginTask = null;
+            //stop the progress spinner
+            showProgress(false);
+
+            if (success) {
+                //  login success and move to driver activity.
+            } else {
+                // login failure
+                passwordTextView.setError(getString(R.string.incorrect_password));
+                passwordTextView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            userLoginTask = null;
+            showProgress(false);
+        }
+    }
+}
